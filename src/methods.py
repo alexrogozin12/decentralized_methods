b7f7bc0cd601f300bc7a3f924628818a66b058e9
lukoshkin <lukoshkin@phystech.edu>
2020-10-23 19:10:55 +0300

    Add ability to continue from args

--- src/methods.py
+++ src/methods.py
@@ -30,30 +30,23 @@ class DGMBase:
         raise NotImplementedError
 
     def _stateInit(self, X0):
+        self._initLogs(X0)
         raise NotImplementedError
 
     def _step(self, X0, *args):
         raise NotImplementedError
 
-    def _consensus(self, X0, *args):
-        cT = self._cT + self.con_iters
-        self._cT = cT
-
-        self.logs['nmix'].append(cT)
-        return self._consensusUpdate(X0, *args)
-
     def _consensusUpdate(self, X0):
         W = self.gen()
         X1 = W @ X0
         return X1
-
-    def _updateStepsize(self, k):
-        pass
         
-    def _initLogs(self):
+    def _initLogs(self, X0):
         self.logs = {'i': [], 'fn': [], 'dist2con': [], 'nmix': []}
         self._cT = 0
-        self._k = 0
+
+        self._record(X0, 0)
+        self._k = 1
         
     def _dist2consensus(self, X):
         return X.var(dim=0, unbiased=False).sum() * self.F.b.size(0)
@@ -61,23 +54,25 @@ class DGMBase:
     def _record(self, X, k):
         self.logs['dist2con'].append(self._dist2consensus(X).item())
         self.logs['fn'].append(self.F(X.mean(0)).item())
+        self.logs['nmix'].append(self._cT)
         self.logs['i'].append(k)
 
     # FIXME: add ability to continue from args
-    def run(self, X0, n_iters=100, lp=1, **kwargs):
+    def run(self, X0, *args, n_iters=100, lp=1, **kwargs):
         """
         Params:
         -------
             X0: torch.Tensor of shape (n, d),
                 n - num of nodes (agents), d - data dimensionality.
         """
-        if kwargs: args = self._args(kwargs)
+        if args: pass
+        elif kwargs: args = self._args(kwargs)
         else: args = self._stateInit(X0)
 
         for k in range(self._k, self._k+n_iters):
             X0, *args = self._step(X0, *args)
+            self._cT += self.con_iters
             if k%lp == 0: self._record(X0, k)
-            self._updateStepsize(k)
         self._k += n_iters
 
         ## Next line works only in python >= 3.8
@@ -99,13 +94,14 @@ class EXTRA(DGMBase):
         return G0, X1
 
     def _stateInit(self, X0):
-        self._initLogs()
+        self._initLogs(X0)
         X0.requires_grad_(True)
         G0 = D(self.F(X0), X0)
         with torch.no_grad():
-            Y = self._consensus(X0)
+            Y = self._consensusUpdate(X0)
             X1 = Y - self.eta*G0
-        self._record(X1, 0)
+        self._record(X1, 1)
+        del self.logs['nmix'][-1]
         self._k += 1
 
         return G0, X1
@@ -115,7 +111,7 @@ class EXTRA(DGMBase):
         G1 = D(self.F(X1), X1)
         with torch.no_grad():
             Y0 = X1 - .5*X0
-            Y1 = self._consensus(Y0)
+            Y1 = self._consensusUpdate(Y0)
             X2 = Y0 + Y1 - self.eta*(G1-G0)
 
         return X1, G1, X2
@@ -131,7 +127,7 @@ class DIGing(DGMBase):
         return G0, Y0
 
     def _stateInit(self, X0):
-        self._initLogs()
+        self._initLogs(X0)
         X0.requires_grad_(True)
         Y0 = D(self.F(X0), X0)
         G0 = Y0.clone()
@@ -139,7 +135,7 @@ class DIGing(DGMBase):
 
     def _step(self, X0, G0, Y0):
         with torch.no_grad():
-            WX, WY = self._consensus(X0, Y0)
+            WX, WY = self._consensusUpdate(X0, Y0)
             X1 = WX - self.eta*Y0
             
         X1.requires_grad_(True)
@@ -164,18 +160,15 @@ class DSGD(DGMBase):
         return ()
 
     def _stateInit(self, X0):
-        self._initLogs()
+        self._initLogs(X0)
         return ()
 
-    def _updateStepsize(self, k):
-        self.eta = self._eta # / (1+k)**.5
-
-    def _step(self, X0, *args):
+    def _step(self, X0):
         X0.requires_grad_(True)
         G = D(self.F(X0), X0)
-        W = self.gen()
         with torch.no_grad():
-            X1 = W @ (X0-self.eta*G)
+            X1 = self._consensusUpdate(X0-self.eta*G)
+        self.eta = self._eta # / (1+k)**.5
         return (X1,)
 
     
@@ -211,7 +204,7 @@ class DAccGD(DGMBase):
         return A0, U0
 
     def _stateInit(self, X0):
-        self._initLogs()
+        self._initLogs(X0)
         A0, U0 = 0, X0.clone()
         return A0, U0
 
@@ -227,7 +220,7 @@ class DAccGD(DGMBase):
         with torch.no_grad():
             V = self.mu*Y + (1+A0*self.mu)*U0 - a*G
             V /= 1 + A0*self.mu + self.mu
-            U1 = self._consensus(V)
+            U1 = self._consensusUpdate(V)
             X1 = (a*U1 + A0*X0) / A1
 
         return X1, A1, U1
@@ -280,7 +273,7 @@ class Mudag(DGMBase):
         return Y0, G0, Y1
 
     def _stateInit(self, X0):
-        self._initLogs()
+        self._initLogs(X0)
         X0 = X0.clone().mean(0).expand(len(X0), -1)
         Y0 = X0.clone()
         G0 = torch.zeros_like(Y0)
@@ -292,7 +285,8 @@ class Mudag(DGMBase):
         G1 = D(self.F(Y1), Y1)
 
         with torch.no_grad():
-            X1 = self._consensus(Y1+X0-Y0-self.eta*(G1-G0))
+            X1 = Y1 + X0 - Y0 - self.eta*(G1-G0)
+            X1 = self._consensusUpdate(X1)
             Y2 = X1 + self._alpha*(X1-X0)
 
         return X1, Y1, G1, Y2
@@ -381,8 +375,8 @@ class APM_C(DGMBase):
         self.beta = beta
 
     def _stateInit(self, X0):
-        self._initLogs()
-        X1, k = X0.clone(), 0
+        self._initLogs(X0)
+        X1, k = X0.clone(), 1
         return X1, k
 
     def _args(self, kwargs):
@@ -396,7 +390,7 @@ class APM_C(DGMBase):
         G = D(self.F(Y), Y)
 
         with torch.no_grad(): Z0 = Y - self.eta*G
-        Z1 = self._consensus(Z0, k)
+        Z1 = self._consensusUpdate(Z0, k)
 
         upsilon = (1 - self.theta)**(k+1)
         X2 = upsilon*Z1 + self.beta*Z0
